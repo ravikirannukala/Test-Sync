eb7344b46c6267ec50e019ebc0ccbe1c605d1931
rnuk0001 <Ravi.Nukala@monash.edu>
2018-05-01 22:47:56 +1000

    new commit

--- index.py
+++ index.py
@@ -1,13 +1,13 @@
 import json
 import datetime
+import requests
 
 
 def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat(),
-        'test':'myTest'
-    }
+    response = requests.get('http://www.mocky.io/v2/5ae856682d0000d2077b47dd',stream=True)
+
+    contentType = response.headers['content-type']
+    data = response.json()
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
