6fba47017695db75b92e952455a0d7ac07a6060f
rnuk0001 <Ravi.Nukala@monash.edu>
2018-05-02 14:55:58 +1000

    test

--- index.py
+++ index.py
@@ -6,9 +6,12 @@ import datetime
 
 def handler(event, context):
     response = requests.get('http://www.mocky.io/v2/5ae856682d0000d2077b47dd',stream=True)
-
     contentType = response.headers['content-type']
     data = response.json()
+    print data
+    for i in data['records']:
+        print "myid" + i.id
+        print "myname" + i.id
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
