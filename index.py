b3f9e3d0a0793959a733324b62da55b55b4f797a
rnuk0001 <Ravi.Nukala@monash.edu>
2018-05-03 09:00:29 +1000

    included the insert

--- index.py
+++ index.py
@@ -1,25 +1,24 @@
 from botocore.vendored import requests
 import json
 import datetime
-import mysql.connector
+import pymysql
 
 
 def handler(event, context):
     response = requests.get('http://www.mocky.io/v2/5ae856682d0000d2077b47dd',stream=True)
     contentType = response.headers['content-type']
     data = response.json()
-    print data
-    for i in data['records']:
-        print "myid" + i['id']
-        print "myname" + i['name']
-    cnx = mysql.connector.connect(user='ravi', password='test1234',
+    cnx = pymysql.connect(user='ravi', password='test1234',
                                   host='devdb.cfe6bx1xwtmf.ap-southeast-2.rds.amazonaws.com',
                                   database='developer')
-    cur = con.cursor()
+    cur = cnx.cursor()
     # Reset Identity Coulmn
-    statement = 'select * from developer'
-    cur.execute(statement)
+    statement = "insert into `developer_details` (`id`,`name`) VALUES (%s,%s)"
+    for i in data['records']:
+        print i['id'] + i['name']
+        cur.execute(statement,(i['id'],i['name']))
     result = cur.fetchall()
+    cnx.commit()
     cnx.close()
     return {'statusCode': 200,
             'body': json.dumps(result),
