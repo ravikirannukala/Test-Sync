9ad9a44c861ce5c9694247140b126f404fc44167
rnuk0001 <Ravi.Nukala@monash.edu>
2018-05-09 12:09:36 +1000

    Added orcl code to connect to db

--- index.py
+++ index.py
@@ -1,16 +1,15 @@
 from botocore.vendored import requests
 import json
 import datetime
-import pymysql
+import cx_Oracle
 
 
 def handler(event, context):
     response = requests.get('http://www.mocky.io/v2/5ae856682d0000d2077b47dd',stream=True)
     contentType = response.headers['content-type']
     data = response.json()
-    cnx = pymysql.connect(user='ravi', password='test1234',
-                                  host='devdb.cfe6bx1xwtmf.ap-southeast-2.rds.amazonaws.com',
-                                  database='developer')
+    cnx = cx_Oracle.connect('hack', 'researchhack',
+                                  host='myreaserch.cfe6bx1xwtmf.ap-southeast-2.rds.amazonaws.com:1521/ORCL')
     cur = cnx.cursor()
     # Reset Identity Coulmn
     statement = "insert into `developer_details` (`id`,`name`) VALUES (%s,%s)"
